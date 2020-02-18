774297925efa14102a38f9c2c154e51eadb76179
Braden <fish2355@hotmail.com>
2020-02-17 21:14:17 -0700

    added restricted middleware

--- server/auth/login-auth.js
+++ server/auth/login-auth.js
@@ -1,3 +1,6 @@
+const db = require("../../data/auth1-model.js");
+const bcrypt = require("bcryptjs");
+
 module.exports = function authUser(req, res, next) {
   const { user, password } = req.body;
   db.getByUser(user).then(user => {
--- server/server.js
+++ server/server.js
@@ -2,17 +2,32 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 const db = require("../data/auth1-model");
 const authUser = require("./auth/login-auth");
 const newUserAuth = require("./auth/register-auth");
+const restricted = require("./auth/restricted-auth");
 
 const server = express();
+const sessionConfig = {
+  name: "monkey", // default "sid"
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 1000 * 30,
+    secure: false, //true in production
+    httpOnly: true //javascript can't access cookie
+  },
+  reSave: false, //same session, don't reSave
+  saveUninitialized: false //GDPR laws against setting cookies automatically
+};
+
 server.use(express.json());
 server.use(morgan("combined"));
 server.use(helmet());
+server.use(session(sessionConfig));
 
-server.get("/api/users", (req, res) => {
+server.get("/api/users", restricted, (req, res) => {
   db.getUsers()
     .then(users => res.status(200).json(users))
     .catch(err =>
@@ -20,7 +35,7 @@ server.get("/api/users", (req, res) => {
     );
 });
 
-server.get("/api/users/:id", (req, res) => {
+server.get("/api/users/:id", restricted, (req, res) => {
   const { id } = req.params;
   db.getUserById(id)
     .then(user =>
@@ -46,11 +61,27 @@ server.post("/api/register", newUserAuth, (req, res) => {
 
 server.post("/api/login", authUser, (req, res) => {
   const { user } = req.body;
+
   db.getByUser(user)
-    .then(user => res.status(200).json({ message: `Welcome ${user.user}` }))
+    .then(user => {
+      req.session.user = user.id;
+      res.status(200).json({ message: `Welcome ${user.user}` });
+    })
     .catch(err =>
       res.status(500).json({ message: "unable to login", error: err })
     );
+});
+
+server.get("/api/logout", (req, res) => {
+  if (req.session.user) {
+    req.session.destroy(err =>
+      err
+        ? res.json({ message: "you can never leave" })
+        : res.status(200).json({ message: "logout was successful" })
+    );
+  } else {
+    res.status(200).json({ message: "You were never here to start" });
+  }
 });
 
 server.use("/", (req, res) => {
